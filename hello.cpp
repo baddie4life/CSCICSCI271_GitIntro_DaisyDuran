c285f6d968ee404014ed8de84f9a3d8d71913fcc
baddie4life <daisyduran168@gmail.com>
2026-05-18 22:58:15 -0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// CSCI 27X - GitHub Upload Practice
+// Name: Daisy Duran
+// Section: 5132
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello from GitHub! My first commit works." << endl;
+    return 0;
+}
